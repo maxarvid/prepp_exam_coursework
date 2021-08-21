5ec7d03fa81d6e2bc3d8802c725b1a5367a47a43
maxarvid <max.arvid.anderson@gmail.com>
2021-08-21 18:32:18 +0200

    add button test implementation, fix linking bug in index.html

--- cypress/integration/userCanClickButton.feature.js
+++ cypress/integration/userCanClickButton.feature.js
@@ -1,8 +1,8 @@
-describe('user can click the button and something happens', () => {
-  it('user clicks button and sees something', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('#message').should('be.empty')
-    cy.get('#quote').click()
-    cy.get('#message').should('contain', 'hello how are you?')
+describe("user can click the button and something happens", () => {
+  it("user clicks button and sees something", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("#message").should("be.empty");
+    cy.get("#quote").click();
+    cy.get("#message").should("contain", "hello how are you");
   });
 });
