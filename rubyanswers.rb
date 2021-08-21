70ab297007479a69aaf6a350421b67abe36bff32
maxarvid <max.arvid.anderson@gmail.com>
2021-08-21 17:11:30 +0200

    add rubyanswers file and first two questions

---
+++ rubyanswers.rb
@@ -0,0 +1,9 @@
+# 1 Create a variable and set it equal to 'variable'. What kind of data type is this?
+
+var = 'variable'
+
+puts "The variable, which I've called var, has a class of #{var.class}."
+
+# 2 Write 5 examples of other data types in Ruby in your Ruby file.
+
+puts "5 other examples of data types are: #{0.1.class}, #{["an array", "of strings"].class}, #{true.class}, hashes, and symbols."
